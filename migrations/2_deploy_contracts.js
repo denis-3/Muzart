1c53ed842794682cecde4cfe874265e1fadcab83
denis-levine <62725075+denis-levine@users.noreply.github.com>
2022-05-23 14:14:19 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
-var Muzart = artifacts.require("Muzart");
+const MuzartProtocol = artifacts.require("MuzartProtocol");
+const Muzcoin = artifacts.require("MUZToken");
+const MuzartNFT = artifacts.require("MuzartNFT");
 
 module.exports = function(deployer) {
-  deployer.deploy(Muzart);
+  deployer.deploy(Muzcoin);
+  deployer.deploy(MuzartProtocol);
+  deployer.deploy(MuzartNFT);
 };
+
+/* A note about deploying it on-chain:
+Deploy Muzcoin.sol first, then set its address in other contracts.
+Next, deploy Muzart.sol
+Finally, deploy MuzartNFT.
+*/
